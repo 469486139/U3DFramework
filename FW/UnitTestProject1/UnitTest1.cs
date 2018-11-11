c5abab311774f5b79899c63ecbe03246bbf7114e
wmm <469486139@qq.com>
2018-11-11 14:01:53 +0800

    test

---
+++ FW/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FW.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			DoubleBuff<uint> buffer = new DoubleBuff<uint>();
+
+			Assert.IsFalse(buffer.Contain(1));
+			buffer.Add(1);
+			Assert.IsTrue(buffer.Contain(1));
+			buffer.Remove(1);
+			Assert.IsFalse(buffer.Contain(1));
+
+			for (uint i = 0; i < 10; i++)
+				buffer.Add(i);
+
+			int cnt = 0;
+			foreach (var item in buffer)
+			{
+				Assert.IsTrue(item == (uint)cnt);
+				buffer.Add(item + 10);
+				cnt++;
+			}
+
+			Assert.IsTrue(buffer.Contain(19));
+			Assert.IsFalse(buffer.Contain(20));
+		}
+	}
+}
---
+++ FW/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FW.Ntf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Notification<uint> ntf = new Notification<uint>();
+			int times_1 = 0;
+			void Cb1(object o)
+			{
+				times_1++;
+			}
+
+			int times_2 = 0;
+			void Cb2(object o)
+			{
+				times_2++;
+			}
+
+			ntf.Regist(1, Cb1);
+			ntf.Regist(1, Cb1);
+			ntf.Regist(2, Cb1);
+			ntf.Regist(2, Cb2);
+
+			ntf.Send(1, null);
+			ntf.Send(2, null);
+
+			Assert.IsTrue(times_1 == 2);
+			Assert.IsTrue(times_2 == 1);
+		}
+	}
+}
